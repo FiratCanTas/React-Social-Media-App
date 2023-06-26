f46656ee208a81b4fc8324bde8e3e6752c61b8d7
Fırat Can Taş <firatcantass@gmail.com>
2023-06-26 16:55:55 +0300

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import Login from "./pages/login/Login.jsx"
 import './App.css';
+import Register from "./pages/register/Register.jsx";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar.jsx"
+import LeftBar from "./components/leftBar/LeftBar.jsx"
+import RightBar from "./components/rightBar/RightBar.jsx"
+import Home from "./pages/home/Home.jsx"
+import Profile from "./pages/profile/Profile.jsx"
+
+const Layout = ()=> {
+  return(
+    <>
+      <Navbar/>
+      <div style={{ display:"flex" }}>
+        <LeftBar/>
+        <Outlet/>
+        <RightBar/>
+      </div>
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout/>,
+    children: [
+      {
+        path:"/",
+        element: <Home/>
+      },
+      {
+        path:"/profile/:id",
+        element: <Profile/>
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+    
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+    
+  },
+]);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom"
+import "./login.scss"
+
+const Login = () => {
+  return (
+    <div className="login">
+        <div className="card">
+          <div className="left">
+            <h1>Hello World.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid modi assumenda eius facilis voluptatem dignissimos iste minima corrupti porro adipisci.</p>
+            <span>Don't you have an account?</span>
+            <Link to={"/register"}> <button>Register</button></Link>
+            
+          </div>
+          <div className="right">
+            <h1>Login</h1>
+            <form>
+              <input type="text" placeholder="Username"/>
+              <input type="password" placeholder="Password"/>
+              <button>Login</button>
+            </form>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
